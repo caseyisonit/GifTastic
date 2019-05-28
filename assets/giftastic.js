341c448f748d44ec7ab48ae28c44d9c469cfd541
casey is on it <caseyeickhoffdesign@gmail.com>
2019-05-27 21:02:03 -0600

    it works.

--- assets/giftastic.js
+++ assets/giftastic.js
@@ -1,7 +1,7 @@
 // BUTTONS AT THE TOP
 $(document).ready(function () {
     var buttonArea = $("#buttonArea");
-    var topics = ["DOGS", "CATS", "FROGS"];
+    var topics = ["ADORE DELANO", "BIANCA DEL RIO", "BENDELACREME"];
 
     function renderBtn() {
         buttonArea.empty();
@@ -31,32 +31,47 @@ $(document).ready(function () {
     $("#buttonArea").on('click', '.topics', function (event) {
         var query = $(this).data("name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=DJZCSsxnXzdnyZsX1TJJM4xMq0Of9PYi&limit=10";
-        
-        console.log("this is this: " + 3)
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
-            for(i = 0; i < response.data.length; i++){
-            var giphyDiv = $("<div class='giphy'>");
-            var giphyImg = $("<img>");
-            var imgURL = response.data[i].images.original.url;
-            var rating = response.data[i].rating;
-            var pOne = $("<p>").text("Rating: " + rating);
-            giphyImg.attr("src", imgURL);
-            giphyDiv.append(giphyImg);
-            giphyDiv.append(pOne);
-            $("#giphy-window").prepend(giphyDiv);
-        }
+            for (i = 0; i < response.data.length; i++) {
+
+                var card = $("<div class='card'>")
+                var cardImg = $("<img class='card-img-top'>")
+                var cardBody = $("<div class='card-body'>")
+                var imgURL = response.data[i].images.fixed_height_still.url;
+                var rating = response.data[i].rating;
+
+                cardImg.attr("src", imgURL)
+                    .attr("data-still", response.data[i].images.fixed_height_still.url)
+                    .attr("data-animate", response.data[i].images.fixed_height.url)
+                    .attr("data-state", "still")
+                    .addClass("gif");
+                card.html(cardImg);
+                cardBody.text("Rating: " + rating);
+                card.append(cardBody);
+                $("#giphy-window").prepend(card);
+
+            }
         })
     });
 
     // START STOP GIFS
     // click function
     // if else if to display
+    $("#giphy-window").on("click", ".gif", function () {
+        var state = $(this).attr("data-state");
 
-    // api.giphy.com / v1 / gifs / search ? PERAMETERSGOESHERE & api_key=INSERTAPIKEYHERE & limit=10
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
 
 });
